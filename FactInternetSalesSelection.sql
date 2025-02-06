1e25ca4b23e3074843b02df78e299f90e0746c71
Leonardo Ramirez <149964718+LeonardoRamirezz@users.noreply.github.com>
2025-02-06 14:23:47 -0600

    Update FactInternetSalesSelection.sql

--- FactInternetSalesSelection.sql
+++ FactInternetSalesSelection.sql
@@ -1,34 +1,26 @@
--- Select Important Columns FactInternetSales table
-SELECT 
-  [ProductKey], 
-  [OrderDateKey], 
-  [DueDateKey], 
-  [ShipDateKey], 
-  [CustomerKey], 
-  --,[PromotionKey]
-  --,[CurrencyKey]
-  --,[SalesTerritoryKey]
-  [SalesOrderNumber], 
-  --[SalesOrderLineNumber], 
-  --,[RevisionNumber]
-  --,[OrderQuantity], 
-  --,[UnitPrice], 
-  --,[ExtendedAmount]
-  --,[UnitPriceDiscountPct]
-  --,[DiscountAmount] 
-  --,[ProductStandardCost]
-  --,[TotalProductCost] 
-  [SalesAmount] 
-  --,[TaxAmt]
-  --,[Freight]
-  --,[CarrierTrackingNumber] 
-  --,[CustomerPONumber] 
-  --,[OrderDate] 
-  --,[DueDate] 
-  --,[ShipDate] 
-FROM 
-  [AdventureWorksDW2019].[dbo].[FactInternetSales]
-WHERE 
-  LEFT (OrderDateKey, 4) >= YEAR(GETDATE()) -2 --Bring rows 2years behind to budget this year
-  ORDER BY
-	OrderDateKey ASC --Order records
+--Get information about sales
+--CTE to join FactInernetSalesReason and DimSalesReason
+WITH SalesReasons_CTE AS (
+    SELECT 
+        fir.SalesOrderNumber,
+        dsr.SalesReasonReasonType
+    FROM dbo.FactInternetSalesReason AS fir
+    LEFT JOIN dbo.DimSalesReason AS dsr 
+        ON fir.SalesReasonKey = dsr.SalesReasonKey
+)
+SELECT --Query to gather all columns
+    fis.ProductKey,
+    fis.OrderDateKey,
+    fis.CustomerKey,
+    fis.SalesOrderNumber,
+    fis.SalesAmount,
+    fis.UnitPrice,
+    sr.SalesReasonReasonType
+FROM dbo.FactInternetSales AS fis
+LEFT JOIN SalesReasons_CTE AS sr
+    ON fis.SalesOrderNumber = sr.SalesOrderNumber
+WHERE
+	(LEFT (OrderDateKey, 4) >= YEAR(GETDATE()) - 2) --Bring rows only behind from the last 2 years
+	AND
+	(LEFT (OrderDateKey, 4) < 2025) --This DB is fictional so we need to filter incoherent values
+ORDER BY fis.OrderDateKey ASC; --Order from oldest to newest
