9cf1e2ad9f00e6c7615343284660df658476312f
Leonardo Ramirez <149964718+LeonardoRamirezz@users.noreply.github.com>
2025-02-06 14:20:29 -0600

    DimDate.sql
    Gather important columns from DimDate

--- DimDateSelection.sql
+++ DimDateSelection.sql
@@ -2,25 +2,14 @@
 SELECT 
   [DateKey], 
   [FullDateAlternateKey] AS Date, 
-   --[DayNumberOfWeek], 
-  [EnglishDayNameOfWeek] AS Day, 
   [WeekNumberOfYear] AS WeekNum,
-  --[SpanishDayNameOfWeek], 
-  --[FrenchDayNameOfWeek], 
-  --[DayNumberOfMonth], 
-  --[DayNumberOfYear], 
+  [EnglishDayNameOfWeek] AS Day,
   [EnglishMonthName] AS Month, 
   Left([EnglishMonthName], 3) AS MonthShort, -- Useful for visualization later
-  --[SpanishMonthName], 
-  --[FrenchMonthName], 
   [MonthNumberOfYear] AS MonthNo, 
   [CalendarQuarter] AS Quarter, 
   [CalendarYear] AS Year
-  --[CalendarSemester], 
-  --[FiscalQuarter], 
-  --[FiscalYear], 
-  --[FiscalSemester] 
 FROM 
-	dbo.DimDate
+  dbo.DimDate
 WHERE 
-	CalendarYear >= 2023 --Analyze 2 years back to budget this year
+  CalendarYear >= 2023 AND CalendarYear <2025;--Analyze 2 years back to budget this year and filter incoherent columns
